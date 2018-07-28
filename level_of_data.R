4c9892ccdc6bf82fb43b0900f6d6ac1b73fab976
Ashrith <ashrith@themathcompany.com>
2018-07-28 19:02:50 +0530

    Added Required Packages

--- level_of_data.R
+++ level_of_data.R
@@ -1,4 +1,20 @@
-library(dplyr)
+#### Setting Up ####
+if(!require("data.table")){install.packages("data.table",dependencies = T);library("data.table")}
+if(!require("dplyr")){install.packages("dplyr",dependencies = T);library("dplyr")}
+if(!require("magrittr")){install.packages("magrittr",dependencies = T);library("magrittr")}
+if(!require("tidyr")){install.packages("tidyr",dependencies = T);library("tidyr")}
+if(!require("plyr")){install.packages("plyr",dependencies = T);library("plyr")}
+if(!require("beepr")){install.packages("beepr",dependencies = T);library("beepr")}
+gc()
+options(scipen = 999)
+setwd("C:/Users/Ashrith Reddy/Desktop/")
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+dev.off()
+options(scipen=999)
+rm(list=ls()); cat("\014")
+
+
+
 
 a = read.csv("C:/Users/Ashrith Reddy/Desktop/Python Preparation/CreditScore.csv")
 if( nrow(unique(a)) == nrow(a)){
