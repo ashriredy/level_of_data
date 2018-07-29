46bdfdf1ae8c51b54aaa6ad8760c7744afdde617
Ashrith <ashrith@themathcompany.com>
2018-07-29 15:38:46 +0530

    Created notes section

--- level_of_data.R
+++ level_of_data.R
@@ -28,7 +28,6 @@ generate_column_combinations = function(dataset,n){
 }
 
 check_for_level = function(dataset , column_combinations){
-   j = 1
   for(j in 1:nrow(column_combinations)){
     concatenated_combination = dataset %>% 
       select(column_combinations[j,]) %>% 
@@ -51,118 +50,7 @@ check_for_level = function(dataset , column_combinations){
 }
 
 #### Determine the level ####
-i = 2
 for(i in 1:ncol(dataset)){
   column_combinations = generate_column_combinations(dataset,i)
   check_for_level(dataset,column_combinations) 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#get column combinations
-# column = combinations  = expand.grid(names(dataset),names(dataset)) %>% apply(MARGIN = 1,sort) %>% t %>% unique %>% as.data.frame() %>% 
-#   filter(apply(.,MARGIN = 1, function(x) length(unique(x))!=1))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#### JUNK ####
-# Pending tasks - 
-# Interactive reader  
-# Missing Values
-# cnames = names(dataset)
-# cnames %>% as.data.frame() %>% setNames(paste0("V",1:length(.)))
-
-
-# add assumptions #
-# csv only?
-
-# if(nrow(unique(a)) == nrow(a)){
-#   unique_rows_flag = T
-# }
-# print(unique_rows_flag)
-
-#efficiency #
-data.table
-gc
-unique fast
-fast accessging a column - check with microbenchmarking
-
-#readability
-# make a function out of everything
-# c1 x c2 x c3 - residual: 0  # insert tabs instead of space
-# aesthetics
-#print in red
-
-# selecting columns is being bottleneck
-# column names as first row
-# if c1 x c2 is a level, c1 x c2 x c3 shouldnt be the level
-
-
-# best-practices / tips and tricsk
-# if datasets is large, try removing columns that are obviously not part of level
-# missing values
-
-# Additional features
-# drop columns feature
-
